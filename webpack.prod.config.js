eb67dbfd8baf1cdfbeae5ce732ac7d50c4accce4
kobekovich <d.v.kove@gmail.com>
2020-01-16 10:15:47 +0200

    initial commit

---
+++ webpack.prod.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    devtool: 'cheap-module-source-map',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        chunkFilename: '[id].js',
+        publicPath: ''
+    },
+    resolve: {
+        extensions: ['.js','.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                 'style-loader',
+                  { loader: 'css-loader', options: { modules: true, importLoaders: 1 } },
+                  { loader: 'postcss-loader', options: { plugins: () => [require('autoprefixer')] } }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'url-loader?limit=8000&name=images/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        new UglifyJsPlugin()
+    ]
+};
